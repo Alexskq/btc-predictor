82b6feffb0b3e38f512deefdf9b5cd20b6e3bc3d
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2024-02-28 15:32:12 +0100

    added timer component

--- app/game/Score.tsx
+++ app/game/Score.tsx
@@ -1,23 +1,8 @@
-// import { PrismaClient } from "@prisma/client";
-
-// const prisma = new PrismaClient();
-
-// export default async function getScore(userId: string) {
-//   const user = await prisma.user.findUnique({
-//     where: {
-//       id: userId,
-//     },
-//   });
-
-//   return user?.score;
-// }
-
-// // Utilisez la fonction
-// const score = await getScore("votre-id-utilisateur");
-// console.log(score);
 import { auth } from "@/src/lib/auth";
 export default async function Score() {
   const session = await auth();
 
-  return <p className="font-semibold text-zinc-800">Score :{user.score}</p>;
+  return (
+    <p className="font-semibold text-zinc-800">Score :{session?.user.score}</p>
+  );
 }
--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,8 +1,10 @@
 import Game from "./Game";
 import Instruction from "./instruction";
 import Score from "./Score";
+import Timer from "./Timer";
 
 import { LogoutButton } from "../AuthButton"; // Import the LogoutButton component
+import { SessionProvider } from "next-auth/react";
 
 export default function Page() {
   return (
@@ -14,6 +16,7 @@ export default function Page() {
       </div>
       <div className="mx-auto bg-white w-[400px] rounded-xl p-4 shadow-md flex justify-between mb-5 border">
         <Score />
+        <Timer />
       </div>
     </div>
   );
