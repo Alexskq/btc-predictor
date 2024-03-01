8149030f284c97bc75dcc1205d2dbcdff95ae33f
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2024-03-01 16:48:08 +0100

    connect api

--- app/game/Game.tsx
+++ app/game/Game.tsx
@@ -1,5 +1,13 @@
-export default function Game() {
-  return <p>Game page component</p>;
+export default async function Game() {
+  // ! connect coinBase api && test it
+  const response = await fetch(
+    "https://api.coindesk.com/v1/bpi/currentprice/BTC.json"
+  );
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
 
-  // ! Connect coinBase api
+  const data = await response.json();
+  console.log(data);
+  return data.bpi.USD.rate_float;
 }
